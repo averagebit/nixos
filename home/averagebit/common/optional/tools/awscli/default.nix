b3b0dc6e70b9df5add831b5a9ea4bf4998ff9b74
averagebit <averagebit@pm.me>
2024-03-24 15:54:42 +0000

    temp rm awscli2

--- home/averagebit/common/optional/tools/awscli/default.nix
+++ home/averagebit/common/optional/tools/awscli/default.nix
@@ -3,7 +3,8 @@
   pkgs,
   ...
 }: {
-  home.packages = with pkgs; [awscli2];
+  # https://github.com/NixOS/nixpkgs/issues/298023
+  # home.packages = with pkgs; [awscli2];
   home.persistence = {
     "/persist/home/${config.home.username}".directories = [".config/aws"];
   };
