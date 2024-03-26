ca041de149e191e335edf453bf5ddfd2fdc57f07
averagebit <averagebit@pm.me>
2024-03-26 12:35:02 +0000

    add sipecUSBRedirection

--- hosts/common/optional/libvirtd.nix
+++ hosts/common/optional/libvirtd.nix
@@ -1,5 +1,9 @@
 {pkgs, ...}: {
-  virtualisation.libvirtd.enable = true;
+  virtualisation = {
+    libvirtd.enable = true;
+    spiceUSBRedirection.enable = true;
+  };
+
   programs.virt-manager.enable = true;
 
   environment.persistence = {
