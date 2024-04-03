2739eb316731f3e516447f383a81b9bfa9417ba0
averagebit <averagebit@pm.me>
2024-04-03 16:54:20 +0000

    sst: 0.0.247 -> 0.0.254

--- pkgs/sst/default.nix
+++ pkgs/sst/default.nix
@@ -6,13 +6,13 @@
 }:
 buildGoModule rec {
   pname = "sst";
-  version = "0.0.247";
+  version = "0.0.254";
 
   src = fetchFromGitHub {
     owner = "sst";
     repo = "ion";
     rev = "v${version}";
-    hash = "sha256-cvuRy4QCY34jG2sOYc6Ur81QUyACxJbKnYhmIOzfl64=";
+    hash = "sha256-CNAMmqLUVU/pg+4EYQd/d3uEr32g8+Tv9qlEp+BmDj0=";
   };
 
   vendorHash = "sha256-F/6VawWgBjpNcjRT2rbf/01Dc5Xzd6KrUD3kFPVUVCQ=";
@@ -23,7 +23,7 @@ buildGoModule rec {
 
   meta = with lib; {
     homepage = "https://github.com/sst/ion";
-    description = "Ion is a new engine for deploying SST apps. It uses Pulumi and Terraform, as opposed to CDK and CloudFormation..";
+    description = "Ion is a new engine for deploying SST apps. It uses Pulumi and Terraform, as opposed to CDK and CloudFormation.";
     mainProgram = "sst";
     maintainers = with maintainers; [averagebit];
     platforms = platforms.unix;
