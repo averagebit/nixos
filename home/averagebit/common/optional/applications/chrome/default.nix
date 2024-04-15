f851d11808caed90add7c40e1d7b541fd64d452c
averagebit <averagebit@pm.me>
2024-04-15 10:15:22 +0000

    apps: persist chrome config

--- home/averagebit/common/optional/applications/chrome/default.nix
+++ home/averagebit/common/optional/applications/chrome/default.nix
@@ -1,3 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [google-chrome];
+
+  home.persistence = {
+    "/persist/home/${config.home.username}".directories = [".config/google-chrome"];
+  };
 }
