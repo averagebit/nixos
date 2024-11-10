7002956134abb02b0444d8075897b4b3127a6780
averagebit <averagebit@pm.me>
2024-11-10 12:01:31 +0000

    pkgs: rm sst

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,7 +3,6 @@
   clone = pkgs.callPackage ./clone {};
   ix = pkgs.callPackage ./ix {};
   myip = pkgs.callPackage ./myip {};
-  sst = pkgs.callPackage ./sst {};
   urlencode = pkgs.callPackage ./urlencode {};
   yank = pkgs.callPackage ./yank {};
 }
