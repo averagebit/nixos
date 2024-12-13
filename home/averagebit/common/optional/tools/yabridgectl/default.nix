d0f6cbefa59fa2fff3936af00f5e1428043bb987
averagebit <averagebit@pm.me>
2024-12-13 05:58:46 +0000

    tools: add yabridge folder

--- home/averagebit/common/optional/tools/yabridgectl/default.nix
+++ home/averagebit/common/optional/tools/yabridgectl/default.nix
@@ -3,9 +3,12 @@
   pkgs,
   ...
 }: {
-  home.packages = with pkgs; [yabridgectl];
+  home.packages = with pkgs; [
+    (yabridgectl.override {wine = wineWowPackages.waylandFull;})
+  ];
   home.persistence = {
     "/persist/home/${config.home.username}".directories = [
+      ".config/yabridge"
       ".config/yabridgectl"
       ".vst3"
     ];
