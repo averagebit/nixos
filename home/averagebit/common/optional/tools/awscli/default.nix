451f7c380c22e1c04e8279aef32eb84731824f6c
averagebit <averagebit@pm.me>
2025-01-12 17:39:48 +0000

    tools: re-add awscli

--- home/averagebit/common/optional/tools/awscli/default.nix
+++ home/averagebit/common/optional/tools/awscli/default.nix
@@ -3,8 +3,7 @@
   pkgs,
   ...
 }: {
-  # https://github.com/NixOS/nixpkgs/issues/298023
-  # home.packages = with pkgs; [awscli2];
+  home.packages = with pkgs; [awscli2];
   home.persistence = {
     "/persist/home/${config.home.username}".directories = [".config/aws"];
   };
