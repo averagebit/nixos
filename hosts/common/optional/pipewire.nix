00760bfbb284510e5844eab600ee0b2b2c7be0b6
averagebit <averagebit@pm.me>
2025-01-20 15:23:12 +0000

    pw: fix deprecated option

--- hosts/common/optional/pipewire.nix
+++ hosts/common/optional/pipewire.nix
@@ -1,12 +1,14 @@
 {
   security.rtkit.enable = true;
-  hardware.pulseaudio.enable = false;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-    wireplumber.enable = true;
+  services = {
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      jack.enable = true;
+      pulse.enable = true;
+      wireplumber.enable = true;
+    };
   };
 }
