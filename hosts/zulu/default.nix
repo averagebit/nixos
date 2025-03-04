ce3a928226ee42637202f38926be23ad417e7522
averagebit <averagebit@pm.me>
2025-03-04 20:17:54 +0000

    zulu: addd tlp, audio, waydroid, wireshark, v4l2

--- hosts/zulu/default.nix
+++ hosts/zulu/default.nix
@@ -13,9 +13,14 @@
     ../common/optional/opengl.nix
     ../common/optional/pipewire.nix
     ../common/optional/podman.nix
+    ../common/optional/rt-audio.nix
     ../common/optional/steam-hardware.nix
     ../common/optional/thunar.nix
+    ../common/optional/tlp.nix
+    ../common/optional/v4l2loopback.nix
     ../common/optional/vial.nix
+    ../common/optional/waydroid.nix
+    ../common/optional/wireshark.nix
     ../common/optional/yubikey.nix
   ];
 
