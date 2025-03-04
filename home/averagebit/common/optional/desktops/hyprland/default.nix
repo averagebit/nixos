1a76c864ea477866c007aff7d5d3123ae6183263
averagebit <averagebit@pm.me>
2025-03-04 20:02:39 +0000

    hyprland: refactor keybinds

--- home/averagebit/common/optional/desktops/hyprland/default.nix
+++ home/averagebit/common/optional/desktops/hyprland/default.nix
@@ -7,6 +7,7 @@
   imports = [
     ../common/global
     ../common/optional/wayland
+    ./basic-binds.nix
   ];
 
   xdg.portal = {
@@ -148,11 +149,6 @@
         ];
       };
 
-      bindm = [
-        "SUPER,mouse:272,movewindow"
-        "SUPER,mouse:273,resizewindow"
-      ];
-
       bind = let
         makoctl = "${config.services.mako.package}/bin/makoctl";
         playerctl = "${config.services.playerctld.package}/bin/playerctl";
@@ -168,84 +164,11 @@
         wpctl = "${pkgs.wireplumber}/bin/wpctl";
       in
         [
-          "SUPERSHIFT,E,exit,"
-          "SUPERSHIFT,Q,killactive,"
-
-          "SUPER,S,togglesplit"
-          "SUPER,F,fullscreen"
-          "SUPERSHIFT,SPACE,togglefloating"
-
-          "SUPER,MINUS,splitratio,-0.25"
-          "SUPERSHIFT,MINUS,splitratio,-0.3333333"
-
-          "SUPER,EQUAL,splitratio,0.25"
-          "SUPERSHIFT,EQUAL,splitratio,0.3333333"
-
-          "SUPER,G,togglegroup"
-          "SUPER,T,lockactivegroup,toggle"
-          "SUPER,APOSTROPHE,changegroupactive,f"
-          "SUPERSHIFT,APOSTROPHE,changegroupactive,b"
-
-          "SUPER,U,togglespecialworkspace"
-          "SUPERSHIFT,U,movetoworkspacesilent,special"
-
-          "SUPER,LEFT,movefocus,l"
-          "SUPER,RIGHT,movefocus,r"
-          "SUPER,UP,movefocus,u"
-          "SUPER,DOWN,movefocus,d"
-
-          "SUPERSHIFT,LEFT,movewindow,l"
-          "SUPERSHIFT,RIGHT,movewindow,r"
-          "SUPERSHIFT,UP,movewindow,u"
-          "SUPERSHIFT,DOWN,movewindow,d"
-
-          "SUPER,1,workspace,1"
-          "SUPER,2,workspace,2"
-          "SUPER,3,workspace,3"
-          "SUPER,4,workspace,4"
-          "SUPER,5,workspace,5"
-          "SUPER,6,workspace,6"
-          "SUPER,7,workspace,7"
-          "SUPER,8,workspace,8"
-          "SUPER,9,workspace,9"
-          "SUPER,0,workspace,10"
-          "SUPER,F1,workspace,11"
-          "SUPER,F2,workspace,12"
-          "SUPER,F3,workspace,13"
-          "SUPER,F4,workspace,14"
-          "SUPER,F5,workspace,15"
-          "SUPER,F6,workspace,16"
-          "SUPER,F7,workspace,17"
-          "SUPER,F8,workspace,18"
-          "SUPER,F9,workspace,19"
-          "SUPER,F10,workspace,20"
-
-          "SUPERSHIFT,1,movetoworkspacesilent,1"
-          "SUPERSHIFT,2,movetoworkspacesilent,2"
-          "SUPERSHIFT,3,movetoworkspacesilent,3"
-          "SUPERSHIFT,4,movetoworkspacesilent,4"
-          "SUPERSHIFT,5,movetoworkspacesilent,5"
-          "SUPERSHIFT,6,movetoworkspacesilent,6"
-          "SUPERSHIFT,7,movetoworkspacesilent,7"
-          "SUPERSHIFT,8,movetoworkspacesilent,8"
-          "SUPERSHIFT,9,movetoworkspacesilent,9"
-          "SUPERSHIFT,0,movetoworkspacesilent,10"
-          "SUPERSHIFT,F1,movetoworkspacesilent,11"
-          "SUPERSHIFT,F2,movetoworkspacesilent,12"
-          "SUPERSHIFT,F3,movetoworkspacesilent,13"
-          "SUPERSHIFT,F4,movetoworkspacesilent,14"
-          "SUPERSHIFT,F5,movetoworkspacesilent,15"
-          "SUPERSHIFT,F6,movetoworkspacesilent,16"
-          "SUPERSHIFT,F7,movetoworkspacesilent,17"
-          "SUPERSHIFT,F8,movetoworkspacesilent,18"
-          "SUPERSHIFT,F9,movetoworkspacesilent,19"
-          "SUPERSHIFT,F10,movetoworkspacesilent,20"
-
           # Applications
           "SUPER,Return,exec,${terminal}"
           "SUPER,E,exec, ${explorer}"
           "SUPER,D,exec, pkill fuzzel || ${menu}"
-          "SUPER,c,exec,${hyprpicker} -a"
+          "SUPER,C,exec,${hyprpicker} -a"
 
           # Brightness
           ",XF86MonBrightnessDown,exec,${brightnessctl} set -5%"
@@ -262,6 +185,12 @@
           "CONTROL,P,exec,${grimblast} --notify --freeze copy screen"
           "SUPER,P,exec,${grimblast} --notify --freeze copy area"
         ]
+        # Lock screen
+        ++ (lib.optionals config.programs.swaylock.enable
+          ["SUPER,L,exec,${swaylock}"])
+        # Notifications
+        ++ (lib.optionals config.services.mako.enable
+          ["SUPER,W,exec,${makoctl} dismiss"])
         # Media control
         ++ (lib.optionals config.services.playerctld.enable [
           ",XF86AudioNext,exec,${playerctl} next"
@@ -271,31 +200,7 @@
           "ALT,XF86AudioNext,exec,${playerctld} shift"
           "ALT,XF86AudioPrev,exec,${playerctld} unshift"
           "ALT,XF86AudioPlay,exec,systemctl --user restart playerctld"
-        ])
-        # Lock screen
-        ++ (lib.optionals config.programs.swaylock.enable
-          ["SUPER,L,exec,${swaylock}"])
-        # Notifications
-        ++ (lib.optionals config.services.mako.enable
-          ["SUPER,W,exec,${makoctl} dismiss"]);
+        ]);
     };
-
-    extraConfig = ''
-      # Resize
-      bind=SUPER,R,submap,Resize
-      submap=Resize
-      binde=,DOWN,resizeactive,0 10
-      binde=,LEFT,resizeactive,-10 0
-      binde=,RIGHT,resizeactive,10 0
-      binde=,UP,resizeactive,0 -10
-      bind=,ESCAPE,submap,reset
-      submap=reset
-
-      # Passthrough
-      bind=SUPER,Z,submap,Passthrough
-      submap=Passthrough
-      bind=SUPER,Z,submap,reset
-      submap=reset
-    '';
   };
 }
