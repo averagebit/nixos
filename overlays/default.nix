4a0958a42755036fbcf7237a3149af38314ceb32
averagebit <averagebit@pm.me>
2025-03-04 20:08:49 +0000

    overlays: yabridge wine hotfix

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,8 @@
-{inputs, ...}: let
+{
+  inputs,
+  outputs,
+  ...
+}: let
   addPatches = pkg: patches:
     pkg.overrideAttrs (oldAttrs: {
       patches = (oldAttrs.patches or []) ++ patches;
@@ -26,17 +30,43 @@ in {
     stable = inputs.nixpkgs-stable.legacyPackages.${final.system};
   };
 
+  # Add custom packages
   additions = final: prev:
     import ../pkgs {pkgs = final;}
     // {
       formats = prev.formats // import ../pkgs/formats {pkgs = final;};
     };
 
-  modifications = final: prev: {
+  # Modify existing packages
+  modifications = final: prev: let
+    wine =
+      import
+      (fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/da466ad.tar.gz";
+        sha256 = "04wc7l07f34aml0f75479rlgj85b7n7wy2mky1j8xyhadc2xjhv5";
+      }) {
+        system = final.system;
+        config = {};
+      };
+  in {
     # example = addPatches prev.example [./example.patch];
     #
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
+
+    # https://github.com/NixOS/nixpkgs/issues/375460
+    wineWowPackages =
+      prev.wineWowPackages
+      // {
+        staging = wine.wineWowPackages.waylandFull;
+        stagingFull = wine.wineWowPackages.waylandFull;
+      };
+    yabridge = prev.yabridge.override {
+      wine = wine.wineWowPackages.waylandFull;
+    };
+    yabridgectl = prev.yabridgectl.override {
+      wine = wine.wineWowPackages.waylandFull;
+    };
   };
 }
