9155356b188dfb938ef9f49a72b44e21f8ef86d9
averagebit <averagebit@pm.me>
2025-03-04 19:48:29 +0000

    apps: add discord alts

--- home/averagebit/common/optional/applications/discord/default.nix
+++ home/averagebit/common/optional/applications/discord/default.nix
@@ -3,10 +3,12 @@
   pkgs,
   ...
 }: {
-  home.packages = with pkgs; [vesktop];
+  home.packages = with pkgs; [discord vesktop webcord-vencord];
   home.persistence = {
     "/persist/${config.home.homeDirectory}" = {
       directories = [
+        ".config/discord"
+        ".config/WebCord"
         ".config/vesktop/sessionData"
         ".config/vesktop/settings"
       ];
