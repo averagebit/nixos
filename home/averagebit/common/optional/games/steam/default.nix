2a2ec1782f89e2e0067f374cffda19ee75ec59b0
averagebit <averagebit@pm.me>
2025-04-19 10:27:37 +0000

    steam: update config

--- home/averagebit/common/optional/games/steam/default.nix
+++ home/averagebit/common/optional/games/steam/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: let
   steam-with-pkgs = pkgs.steam.override {
@@ -16,13 +17,41 @@
         stdenv.cc.cc.lib
         libkrb5
         keyutils
+        gamescope
       ];
   };
+
+  monitor = lib.head (lib.filter (m: m.primary) config.monitors);
+  steam-session = let
+    gamescope = lib.concatStringsSep " " [
+      (lib.getExe pkgs.gamescope)
+      "--output-width ${toString monitor.width}"
+      "--output-height ${toString monitor.height}"
+      "--framerate-limit ${toString monitor.refreshRate}"
+      "--prefer-output ${monitor.name}"
+      "--adaptive-sync"
+      "--expose-wayland"
+      "--hdr-enabled"
+      "--steam"
+    ];
+    steam = lib.concatStringsSep " " [
+      "steam"
+      "steam://open/bigpicture"
+    ];
+  in
+    pkgs.writeTextDir "share/wayland-sessions/steam-sesson.desktop" # ini
+    
+    ''
+      [Desktop Entry]
+      Name=Steam Session
+      Exec=${gamescope} -- ${steam}
+      Type=Application
+    '';
 in {
-  home.packages = with pkgs; [
-    gamescope
-    protontricks
-    sc-controller
+  home.packages = [
+    pkgs.gamescope
+    pkgs.protontricks
+    steam-session
     steam-with-pkgs
   ];
   home.persistence = {
