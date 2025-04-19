b2f9ba274133a222fb97865bb1a15ae50e4a6962
averagebit <averagebit@pm.me>
2025-04-19 10:28:06 +0000

    wayland: update env vars

--- home/averagebit/common/optional/desktops/common/optional/wayland/default.nix
+++ home/averagebit/common/optional/desktops/common/optional/wayland/default.nix
@@ -22,13 +22,8 @@
 
   home.sessionVariables = {
     NIXOS_OZONE_WL = 1;
-
-    XDG_SESSION_TYPE = "wayland";
-
+    MOZ_ENABLE_WAYLAND = 1;
     QT_QPA_PLATFORM = "wayland";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = 1;
-
-    WLR_NO_HARDWARE_CURSORS = 1;
   };
 
   xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-wlr];
