29395ffd0fda058cfd17690193bd4181910e353e
averagebit <averagebit@pm.me>
2025-05-03 23:48:31 +0000

    cam: read vars if missing

--- pkgs/cam/cam.sh
+++ pkgs/cam/cam.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+[[ -z "${VCAM_IP}" ]] && read -rp "ADB IP: " VCAM_IP
+[[ -z "${VCAM_DEVICE}" ]] && read -rp "VCAM Device: " VCAM_DEVICE
+
 read -rp "ADB Connection Port: " VCAM_CONNECT_PORT
 if ! adb connect "${VCAM_IP}:${VCAM_CONNECT_PORT}"; then
     read -rp "ADB Pair Port: " VCAM_PAIR_PORT
@@ -8,4 +11,3 @@ if ! adb connect "${VCAM_IP}:${VCAM_CONNECT_PORT}"; then
 fi
 
 exec scrcpy --tcpip --v4l2-sink=/dev/"${VCAM_DEVICE}" --no-video-playback
-
