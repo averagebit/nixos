390b7feebe6d970da3d903ec3ba1ba4146f301d5
averagebit <averagebit@pm.me>
2025-05-03 23:50:59 +0000

    gamemode: rm gpu_optimizations

--- hosts/common/optional/gamemode.nix
+++ hosts/common/optional/gamemode.nix
@@ -7,8 +7,6 @@
         inhibit_screensaver = 1;
       };
       gpu = {
-        apply_gpu_optimisations = "accept-responsibility";
-        gpu_device = 0;
         amd_performance_level = "high";
       };
     };
