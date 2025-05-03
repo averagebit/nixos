823925b25699532a0e202b4798c375892ed12168
averagebit <averagebit@pm.me>
2025-05-03 23:47:40 +0000

    shell: update pkgs import

--- shell.nix
+++ shell.nix
@@ -1,14 +1,4 @@
-{
-  pkgs ? let
-    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
-    nixpkgs = fetchTarball {
-      url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
-      sha256 = lock.narHash;
-    };
-  in
-    import nixpkgs {overlays = [];},
-  ...
-}: {
+{pkgs ? import <nixpkgs> {}, ...}: {
   default = pkgs.mkShell {
     NIX_CONFIG = "extra-experimental-features = nix-command flakes";
     nativeBuildInputs = with pkgs; [
