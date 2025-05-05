4bd4188b256fefaa234d74c4ff887a4a76271c65
averagebit <averagebit@pm.me>
2025-05-05 18:46:51 +0000

    zulu: add monitor

--- home/averagebit/zulu/default.nix
+++ home/averagebit/zulu/default.nix
@@ -7,5 +7,16 @@
     ../common/optional/tools
   ];
 
+  monitors = [
+    {
+      name = "eDP-1";
+      width = 1366;
+      height = 768;
+      workspace = "1";
+      refreshRate = 60;
+      primary = true;
+    }
+  ];
+
   home.stateVersion = lib.mkDefault "23.11";
 }
