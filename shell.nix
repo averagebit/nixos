ed8544cf47c6ea16b4244f9947aac4986d4b7ed3
averagebit <averagebit@pm.me>
2025-05-05 16:35:06 +0000

    shell: add formatter and lsp

--- shell.nix
+++ shell.nix
@@ -2,7 +2,9 @@
   default = pkgs.mkShell {
     NIX_CONFIG = "extra-experimental-features = nix-command flakes";
     nativeBuildInputs = with pkgs; [
+      alejandra
       nix
+      nixd
       home-manager
       git
 
