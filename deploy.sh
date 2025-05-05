df7f83455c4f810d0ca0d4d35a4002816b5d1f79
averagebit <averagebit@pm.me>
2025-05-05 18:46:46 +0000

    deploy: rm setpci

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,10 @@
 hosts="$1"
 shift
 
-setpci
-
-[[ -z "$hosts" ]] && { echo "No hosts to deploy"; exit 1; }
+[[ -z "$hosts" ]] && {
+    echo "No hosts to deploy"
+    exit 1
+}
 
 for host in ${hosts//,/ }; do
     nixos-rebuild switch --flake .\#$host --build-host $host --target-host $host --use-remote-sudo --use-substitutes $@
