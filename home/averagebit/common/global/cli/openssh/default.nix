05ffd16676f00d74bb71ee3f613898aff23cd5f4
averagebit <averagebit@pm.me>
2025-05-15 03:00:32 +0000

    ssh: persist known_hosts as dir

--- home/averagebit/common/global/cli/openssh/default.nix
+++ home/averagebit/common/global/cli/openssh/default.nix
@@ -1,13 +1,13 @@
 {
-  outputs,
   config,
-  lib,
+  outputs,
   ...
 }: let
   hostnames = builtins.attrNames outputs.nixosConfigurations;
 in {
   programs.ssh = {
     enable = true;
+    userKnownHostsFile = "${config.home.homeDirectory}/.ssh/known_hosts.d/hosts";
     matchBlocks = {
       net = {
         host = builtins.concatStringsSep " " hostnames;
@@ -17,16 +17,20 @@ in {
             bind.address = ''/%d/.gnupg-sockets/S.gpg-agent'';
             host.address = ''/%d/.gnupg-sockets/S.gpg-agent.extra'';
           }
+          {
+            bind.address = ''/%d/.waypipe/server.sock'';
+            host.address = ''/%d/.waypipe/client.sock'';
+          }
         ];
-      };
-      trusted = lib.hm.dag.entryBefore ["net"] {
-        host = "averagebit.com *.averagebit.com";
-        forwardAgent = true;
+        forwardX11 = true;
+        forwardX11Trusted = true;
+        setEnv.WAYLAND_DISPLAY = "wayland-waypipe";
+        extraOptions.StreamLocalBindUnlink = "yes";
       };
     };
   };
 
   home.persistence = {
-    "/persist/${config.home.homeDirectory}".directories = [".ssh"];
+    "/persist/${config.home.homeDirectory}".directories = [".ssh/known_hosts.d"];
   };
 }
