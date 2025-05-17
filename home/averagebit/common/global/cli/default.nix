eb0d2c0514396f5c788b688e87c4b3bdd68582f9
averagebit <averagebit@pm.me>
2025-05-17 15:40:01 +0000

    home: add prettierd

--- home/averagebit/common/global/cli/default.nix
+++ home/averagebit/common/global/cli/default.nix
@@ -26,6 +26,7 @@
     nh # Home manager and Nix wrapper
     nix-search-tv # Fuzzy search for NixOS packages
     nixd # Nix LSP
+    prettierd # Common formatter
     urlencode # URI encoder
   ];
 }
