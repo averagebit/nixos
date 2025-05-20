5f5143fc57944e02d13359878bde95e085e8ddae
averagebit <averagebit@pm.me>
2025-05-20 13:16:22 +0000

    feat(prometheus): add 'processes' collector

--- hosts/common/global/prometheus-node-exporter.nix
+++ hosts/common/global/prometheus-node-exporter.nix
@@ -1,7 +1,7 @@
 {config, ...}: {
   services.prometheus.exporters.node = {
     enable = true;
-    enabledCollectors = ["systemd"];
+    enabledCollectors = ["systemd" "processes"];
   };
   networking.firewall.interfaces."tailscale0" = {
     allowedTCPPorts = [config.services.prometheus.exporters.node.port];
