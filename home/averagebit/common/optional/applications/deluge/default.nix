45310da72f4b438cc3a0ccc0942b1e78ced0b4bb
averagebit <averagebit@pm.me>
2025-07-19 11:54:13 +0000

    apps: persist deluge config dir

--- home/averagebit/common/optional/applications/deluge/default.nix
+++ home/averagebit/common/optional/applications/deluge/default.nix
@@ -1,3 +1,12 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   home.packages = with pkgs; [deluge];
+  home.persistence = {
+    "/persist/${config.home.homeDirectory}".directories = [
+      ".config/deluge"
+    ];
+  };
 }
