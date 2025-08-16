2a8dfc470c9b74a5d3e1cb6cbb10ec7c7e1ab4a9
averagebit <averagebit@pm.me>
2025-08-16 14:45:25 +0000

    wopr: rm a monitor

--- home/averagebit/wopr/default.nix
+++ home/averagebit/wopr/default.nix
@@ -16,16 +16,16 @@
       refreshRate = 100;
       primary = true;
     }
-    {
-      name = "DP-1";
-      width = 1680;
-      height = 1050;
-      workspace = "20";
-      refreshRate = 60;
-      positionX = 1920;
-      positionY = 0;
-      enabled = true;
-    }
+    # {
+    #   name = "DP-1";
+    #   width = 1680;
+    #   height = 1050;
+    #   workspace = "20";
+    #   refreshRate = 60;
+    #   positionX = 1920;
+    #   positionY = 0;
+    #   enabled = true;
+    # }
   ];
 
   home.stateVersion = lib.mkDefault "23.11";
