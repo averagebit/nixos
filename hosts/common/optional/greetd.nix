75dee4753c8889da4a2508d626e86f0fa938d917
averagebit <averagebit@pm.me>
2025-08-17 21:30:43 +0000

    greetd: rm vt assign

--- hosts/common/optional/greetd.nix
+++ hosts/common/optional/greetd.nix
@@ -1,10 +1,9 @@
 {pkgs, ...}: {
   services.greetd = {
     enable = true;
-    vt = 2;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --remember-user-session --user-menu --time --time-format '%I:%M %p | %a • %h | %F' --cmd bash";
+        command = "${pkgs.tuigreet}/bin/tuigreet --remember --remember-user-session --user-menu --time --time-format '%I:%M %p | %a • %h | %F' --cmd bash";
       };
     };
   };
