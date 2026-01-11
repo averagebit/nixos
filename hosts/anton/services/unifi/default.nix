2fe5b55c63b33c7399a30958228c02f30816b9bb
averagebit <averagebit@pm.me>
2026-01-11 01:58:31 +0000

    anton: add unifi service

---
+++ hosts/anton/services/unifi/default.nix
@@ -0,0 +1,29 @@
+{pkgs, ...}: {
+  services = {
+    unifi = {
+      enable = true;
+      openFirewall = true;
+      unifiPackage = pkgs.unifi;
+      mongodbPackage = pkgs.mongodb;
+    };
+    nginx.virtualHosts = {
+      "wifi.averagebit.com" = {
+        forceSSL = true;
+        enableACME = true;
+        acmeRoot = null;
+        locations."/" = {
+          proxyPass = "https://localhost:8443";
+          proxyWebsockets = true;
+        };
+      };
+    };
+  };
+  environment.persistence."/persist".directories = [
+    {
+      directory = "/var/lib/unifi";
+      user = "unifi";
+      group = "unifi";
+      mode = "0700";
+    }
+  ];
+}
