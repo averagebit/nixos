7cf13c3e4904550a2297faa09bc9f15744b29d4e
averagebit <averagebit@pm.me>
2026-03-06 18:34:27 +0000

    gpg: rm link gnupg sockets service

--- home/averagebit/common/global/cli/gpg/default.nix
+++ home/averagebit/common/global/cli/gpg/default.nix
@@ -35,19 +35,4 @@
       ];
     };
   };
-
-  systemd.user.services = {
-    link-gnupg-sockets = {
-      Unit = {
-        Description = "link gnupg sockets from /run to /home";
-      };
-      Service = {
-        Type = "oneshot";
-        ExecStart = "${pkgs.coreutils}/bin/ln -Tfs /run/user/%U/gnupg %h/.gnupg-sockets";
-        ExecStop = "${pkgs.coreutils}/bin/rm $HOME/.gnupg-sockets";
-        RemainAfterExit = true;
-      };
-      Install.WantedBy = ["default.target"];
-    };
-  };
 }
